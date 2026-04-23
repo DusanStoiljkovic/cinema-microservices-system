1ea0184abe7e8afe67ac86d91177dc115ae7a972
DusanStoiljkovic <dusanstoiljkovic02@gmail.com>
2026-04-23 16:28:28 +0200

    add authentication and throttling middleware for enhanced security

--- userService/internal/middleware/auth.go
+++ userService/internal/middleware/auth.go
@@ -3,11 +3,18 @@ package middleware
 import (
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
+var (
+	clients = make(map[string]time.Time)
+	mu      sync.Mutex
+)
+
+// JWT TOKEN LOGIC
 func JwtAuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		tokenString := r.Header.Get("Authorization")
@@ -43,4 +50,33 @@ func CreateToken(email string) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+// AUTHENTICATION MIDDLEWARE
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apiKey := r.Header.Get("X-API-Key")
+		if apiKey != os.Getenv("SECRET_KEY") {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// THROTTLE AUTH MIDDLEWARE
+func ThrottleMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
+		defer mu.Unlock()
+		ip := r.RemoteAddr
+		if lastRequest, found := clients[ip]; found && time.Since(lastRequest) < time.Second {
+			http.Error(w, "To many requests", http.StatusTooManyRequests)
+			return
+		}
+
+		clients[ip] = time.Now()
+		next.ServeHTTP(w, r)
+	})
 }
