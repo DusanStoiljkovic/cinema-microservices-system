ba7761e591ee8c00b0f959812c126c05a6ceab71
DusanStoiljkovic <dusanstoiljkovic02@gmail.com>
2026-04-23 15:18:14 +0200

    refactor user routes to include JWT authentication middleware for GetUserByID

--- userService/internal/routes/router.go
+++ userService/internal/routes/router.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"net/http"
 	"user-service/internal/handlers"
+	"user-service/internal/middleware"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -12,9 +13,13 @@ func RegisterRouter(UserHandler *handlers.UserHandler) http.Handler {
 
 	r.Group(func(r chi.Router) {
 		r.Route("/users", func(r chi.Router) {
-			r.Get("/{id}", UserHandler.GetUserByID)
 			r.Post("/register", UserHandler.RegisterUser)
 			r.Post("/login", UserHandler.LoginUser)
+
+			r.Group(func(r chi.Router) {
+				r.Use(middleware.JwtAuthMiddleware)
+				r.Get("/{id}", UserHandler.GetUserByID)
+			})
 		})
 	})
 
